3ba39d8b1222245044fdcd26fcf7228ac4de6351
Jane <maryjanesalem@outlook.ph>
2022-12-12 09:26:44 +0800

    added prompt input for operator

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ let firstNumber = Number(prompt("Enter first number: "));
 
 let secondNumber = Number(prompt("Enter second number: "));
 
+let operator = prompt("Choose your operator: + - * /");
+
+
 const addNum = (num1, num2) => num1 + num2;
 const subtractNum = (num1, num2) => num1 - num2;
 const multiplyNum = (num1, num2) => num1 * num2;
 const divideNum = (num1, num2) => num1 / num2;
 
+if (operator == "+") {
+    alert(`The sum of ${firstNumber} and ${secondNumber} is equal to ${addNum(firstNumber, secondNumber)}.`);
+} else if (operator == "-") {
+    alert(`The difference of ${firstNumber} and ${secondNumber} is equal to ${subtractNum(firstNumber, secondNumber)}.`);
+} else if (operator == "*") {
+    alert(`The product of ${firstNumber} and ${secondNumber} is equal to ${multiplyNum(firstNumber, secondNumber)}.`);
+} else if (operator == "/") {
+    alert(`The quotient of ${firstNumber} and ${secondNumber} is equal to ${divideNum(firstNumber, secondNumber)}.`);
+} else {
+    alert("Wrong input, please refresh the page and try again.");
+}
 
-alert(`The sum of ${firstNumber} and ${secondNumber} is equal to ${addNum(firstNumber, secondNumber)}.
-The difference of ${firstNumber} and ${secondNumber} is equal to ${subtractNum(firstNumber, secondNumber)}.
-The product of ${firstNumber} and ${secondNumber} is equal to ${multiplyNum(firstNumber, secondNumber)}.
-The quotient of ${firstNumber} and ${secondNumber} is equal to ${divideNum(firstNumber, secondNumber)}.
-`);
